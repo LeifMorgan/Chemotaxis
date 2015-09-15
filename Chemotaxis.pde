0de64e47d85b1a5a3ffea044aef9a58fa2d8c488
LeifMorgan <leif.e.morgan@gmail.com>
2015-09-15 10:32:04 -0700

    added first name to index

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,16 +1,26 @@
  //declare bacteria variables here   
+ Bacteria [] colony;
  Bacteria small;
+ int rX = (int)(Math.random()*50-1);
+ int rY = (int)(Math.random()*50-1);
  void setup()   
  {     
  	//initialize bacteria variables here   
  	size(300,300);
  	small = new Bacteria();  
+ 	colony = new Bacteria[20];
+ 	for(int a = 0; a < colony.length; a++){
+ 		colony[a] = new Bacteria();
+ 	}
  }   
  void draw()   
  {    
  	background(0);
-  	small.show();
-  	small.walk();
+ 	for(int a = 0; a < colony.length; a++){
+ 		 colony[a].show();
+ 		 colony[a].walk();
+ 	}
+
  }  
 
 
@@ -25,15 +35,28 @@
  	}
  	void walk()
  	{
- 		myX = myX + (int)(Math.random()*3-1);
- 		myY = myY + (int)(Math.random()*3-1);
+ 		myX = myX + (int)(Math.random()*3-1.5);
+ 		myY = myY + (int)(Math.random()*3-1.5);
+ 		if(myX >= 300){
+ 			myX = myX + (int)(Math.random()*3-3);
+ 		}
+ 		if(myY >= 300){
+ 			myY = myY + (int)(Math.random()*3-3);
+ 		}
+ 		if(myY <= 0){
+ 			myY = myY + (int)(Math.random()*3+1);
+ 		}
+ 		if(myX <= 0){
+ 			myX = myX + (int)(Math.random()*3+1);
+ 		}
  	}
  	void show()
  	{
- 		fill(0,50,200,10);
+ 		fill(51,255,153,35);
  		strokeWeight(1);
- 		stroke(0,0,140);
+ 		stroke(51,255,153);
  		ellipse(myX,myY,10,10);
+ 		rect(myX + rX,myY+rY,10,10);
  	}
 
  }    
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -15,18 +15,28 @@ import java.io.IOException;
 public class Chemotaxis extends PApplet {
 
  //declare bacteria variables here   
+ Bacteria [] colony;
  Bacteria small;
+ int rX = (int)(Math.random()*50-1);
+ int rY = (int)(Math.random()*50-1);
  public void setup()   
  {     
  	//initialize bacteria variables here   
  	size(300,300);
  	small = new Bacteria();  
+ 	colony = new Bacteria[20];
+ 	for(int a = 0; a < colony.length; a++){
+ 		colony[a] = new Bacteria();
+ 	}
  }   
  public void draw()   
  {    
  	background(0);
-  	small.show();
-  	small.walk();
+ 	for(int a = 0; a < colony.length; a++){
+ 		 colony[a].show();
+ 		 colony[a].walk();
+ 	}
+
  }  
 
 
@@ -41,15 +51,28 @@ public class Chemotaxis extends PApplet {
  	}
  	public void walk()
  	{
- 		myX = myX + (int)(Math.random()*3-1);
- 		myY = myY + (int)(Math.random()*3-1);
+ 		myX = myX + (int)(Math.random()*3-1.5f);
+ 		myY = myY + (int)(Math.random()*3-1.5f);
+ 		if(myX >= 300){
+ 			myX = myX + (int)(Math.random()*3-3);
+ 		}
+ 		if(myY >= 300){
+ 			myY = myY + (int)(Math.random()*3-3);
+ 		}
+ 		if(myY <= 0){
+ 			myY = myY + (int)(Math.random()*3+1);
+ 		}
+ 		if(myX <= 0){
+ 			myX = myX + (int)(Math.random()*3+1);
+ 		}
  	}
  	public void show()
  	{
- 		fill(0,50,200,10);
+ 		fill(51,255,153,35);
  		strokeWeight(1);
- 		stroke(0,0,140);
+ 		stroke(51,255,153);
  		ellipse(myX,myY,10,10);
+ 		rect(myX + rX,myY+rY,10,10);
  	}
 
  }    
